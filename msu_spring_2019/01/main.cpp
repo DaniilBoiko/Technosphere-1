c086bce56c356df4c0799fafe1d1c93d32c2464e
Daniil Boiko <daniel.boyko.al@gmail.com>
2019-03-17 19:10:56 +0300

    Fix

--- msu_spring_2019/01/main.cpp
+++ msu_spring_2019/01/main.cpp
@@ -20,7 +20,7 @@ bool is_prime(int number) {
 
 }
 
-int loop(int start, int end) {
+int find_prime_count(int start, int end) {
 	int result = 0;
 	int is_parsing = false;
 
@@ -36,7 +36,7 @@ int loop(int start, int end) {
 				return -1;
 			}
 
-			if (Data[i] == end) {
+			if ((Data[i] == end)&(Data[i+1] != end)) {
 				return result;
 			}
 		} else {
@@ -53,22 +53,15 @@ int loop(int start, int end) {
 }
 
 int main(int argc, char* argv[]) {
-	int args[argc - 1];
-
-	for (int i = 1; i < argc; ++i) {
-		args[i - 1] = std::atoi(argv[i]);
-	}
-
-	argc--;
-
 	// Check for a wrong input
-	if ((argc % 2 != 0)|(argc == 0)) {
+	if (((argc-1) % 2 != 0)|(argc-1 == 0)) {
 		return -1;
 	}
 
 	// Iterate through pairs of values
-	for (int i = 0; i < argc/2; i++) {
-		int n_of_primes = loop(args[i*2],args[i*2+1]);
+	for (int i = 0; i < (argc-1)/2; ++i) {
+		int n_of_primes = find_prime_count(std::atoi(argv[2*i+1]),std::atoi(argv[2*i+2]));
+
 		if (n_of_primes == -1){
 			return -1;
 		}
@@ -77,5 +70,4 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
-	return 0;
 }
