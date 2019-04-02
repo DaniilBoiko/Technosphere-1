0abb03a36c293cfcb2b28402344070d0c74e79e7
Daniil Boiko <daniel.boyko.al@gmail.com>
2019-04-02 11:27:38 +0300

    HW 3

---
+++ msu_spring_2019/03/matrix.h
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+class Matrix {
+	size_t columns;
+	size_t rows;
+	int **content;
+
+public:
+	Matrix(size_t _rows, size_t _columns) :
+			columns(_columns), rows(_rows), content(new int*[rows]) {
+		for (int i = 0; i < rows; i += 1) {
+			content[i] = new int[columns];
+		}
+	}
+
+	Matrix(const Matrix &other) :
+			columns(other.columns), rows(other.rows), content(other.content) {
+	}
+
+	size_t getRows() const {
+		return rows;
+	}
+
+	size_t getColumns() const {
+		return columns;
+	}
+
+	class Row {
+	public:
+		int *content;
+		size_t columns;
+
+		Row(int *row, const int c) :
+				content(row), columns(c) {
+		}
+
+		const int& operator [](size_t i) const {
+			if (i >= columns) {
+				throw std::out_of_range("");
+			} else {
+				return content[i];
+			}
+		}
+
+		int& operator [](size_t i) {
+			if (i >= columns) {
+				throw std::out_of_range("");
+			} else {
+				return content[i];
+			}
+		}
+
+	};
+
+	const Row operator [](const size_t i) const {
+		if (i >= rows) {
+			throw std::out_of_range("");
+		} else {
+			Row row(content[i], columns);
+			return row;
+		}
+	}
+
+	Row operator [](const size_t i) {
+		if (i >= rows) {
+			throw std::out_of_range("");
+		} else {
+			Row row(content[i], columns);
+			return row;
+		}
+	}
+
+	// Равенство
+	bool operator==(const Matrix& other) const {
+		if (this == &other)
+			return true;
+
+		if (rows != other.rows ||  columns != other.columns) {
+			return false;
+		}
+
+		for (size_t i = 0; i < rows; ++i) {
+			for (size_t j = 0; j < columns; ++j) {
+				if (content[i][j] != other.content[i][j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	// Неравенство
+	bool operator!=(const Matrix& other) const {
+		return !(*this == other);
+	}
+
+	// Произведение
+	Matrix& operator*=(int c) {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				content[i][j] *= c;
+			}
+		}
+
+		return *this;
+	}
+
+	~Matrix() {
+		for (int i = 0; i < rows; ++i) {
+			delete[] content[i];
+		}
+		delete[] content;
+	}
+
+};
